a8ed5ef4a4f79e2dab83c0c515f363ca22b2e957
Tyler Kloster <tykl4001@colorado.edu>
2023-04-13 15:56:33 -0600

    Adding Alex's login page and creating a register page and integrating both pages

--- All project code_components/src/index.js
+++ All project code_components/src/index.js
@@ -9,7 +9,6 @@ const bodyParser = require('body-parser');
 const session = require('express-session'); // To set the session object. To store or access session data, use the `req.session`, which is (generally) serialized as JSON by the store.
 const bcrypt = require('bcrypt'); //  To hash passwords
 const axios = require('axios'); // To make HTTP requests from our server. We'll learn more about it in Part B.
-
 // *****************************************************
 // <!-- Section 2 : Connect to DB -->
 // *****************************************************
@@ -67,7 +66,60 @@ app.get('/welcome', (req, res) => {
   res.json({status: 'success', message: 'Welcome!'});
 });
 
+app.get('/', (req, res) => {
+  res.redirect('/login');
+});
 
+app.get('/login', (req, res) => {
+  res.render("pages/login");
+});
+
+app.get('/home', (req, res) => {
+  res.render("pages/home");
+})
+
+app.post('/login', async (req, res) => {
+  const query = `SELECT * FROM users WHERE username = '${req.body.username}';`;
+  db.one(query)
+  .then(async user => {
+    const match = await bcrypt.compare(req.body.password, user.password)
+    if (req.body.password == '' || req.body.password == 'undefined') {
+      console.log("no password");
+    }
+    else if (match) {
+      req.session.user = user;
+      req.session.save();
+      console.log("correct password");
+      res.redirect('/home');
+    }
+    else {
+      console.log("wrong password")
+    }
+  })
+  .catch(err => {
+    return console.log(err);
+  });
+});
+
+app.get('/register', (req, res) => {
+  res.render("pages/register");
+});
+
+app.post('/register', async (req, res) => {
+  const hash = await bcrypt.hash(req.body.password, 10);
+  const username = await req.body.username;
+  console.log(hash);
+
+  const query = "INSERT INTO users (username, password) values ($1, $2);"
+  db.any(query, [username, hash])
+  .then(function (data) {
+      console.log(username, hash);
+      res.render("pages/login");
+  })
+  .catch(function (err) {
+      return console.log(err);
+  });
+});
 
 
 
@@ -75,5 +127,5 @@ app.get('/welcome', (req, res) => {
 // <!-- Section 5 : Start Server-->
 // *****************************************************
 // starting the server and keeping the connection open to listen for more requests
-module.exports = app.listen(3000);
+app.listen(3000);
 console.log('Server is listening on port 3000');
