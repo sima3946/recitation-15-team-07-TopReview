d6365453a9c3fa74062ea2a570b9107c78945bd6
sima3946 <sima3946@colorado.edu>
2023-04-13 13:23:42 -0600

    created new brawnch for top navigation bar

--- All project code_components/scr/init_data/insert.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO users (username, password) VALUES ('Test', 1234);
-INSERT INTO users (username, password) VALUES ('Test2', today);
-INSERT INTO users (username, password) VALUES ('Test3', nothing);
-INSERT INTO users (username, password) VALUES ('Test4', problem);
-INSERT INTO users (username, password) VALUES ('Test5', 12345);
