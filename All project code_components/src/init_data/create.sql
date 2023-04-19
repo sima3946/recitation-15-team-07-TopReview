d49428715b803a9e2aeb1d6b26aa5cd29ac1a26c
guki7733 <guki7733@colorado.edu>
2023-04-19 02:08:45 -0600

    created movie and movieReviews table in create.sql

--- All project code_components/src/init_data/create.sql
+++ All project code_components/src/init_data/create.sql
@@ -2,3 +2,15 @@ CREATE TABLE IF NOT EXISTS users(
     username VARCHAR(50) PRIMARY KEY,
     password CHAR(60) NOT NULL
     );
+
+CREATE TABLE Movies (
+  movie_id INT PRIMARY KEY,
+  name VARCHAR(255)
+);
+
+CREATE TABLE MovieReviews (
+  review_id INT PRIMARY KEY,
+  movie_id INT,
+  review TEXT,
+  FOREIGN KEY (movie_id) REFERENCES Movies(id)
+);
