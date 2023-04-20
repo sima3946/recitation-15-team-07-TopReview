00675f60daa7c0d147544311a82f35c844a7364c
Tyler Kloster <tykl4001@colorado.edu>
2023-04-20 08:54:14 -0600

    staging for merge with main

--- All project code_components/src/index.js
+++ All project code_components/src/index.js
@@ -140,6 +140,6 @@ app.post('/userID', async (req, res) => {
 // <!-- Section 5 : Start Server-->
 // *****************************************************
 // starting the server and keeping the connection open to listen for more requests
-// module.exports = app.listen(3000);
-app.listen(3000);
+module.exports = app.listen(3000);
+// app.listen(3000);
 console.log('Server is listening on port 3000');
