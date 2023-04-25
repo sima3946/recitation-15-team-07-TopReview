fb72dc666756e2414fa610f5c5725443865af669
kive7791 <kvel1097@gmail.edu>
2023-04-25 16:26:59 -0600

    Progress for the review page

--- All project code_components/src/resources/js/search.js
+++
@@ -1,9 +0,0 @@
-const movie = req.body.movie || '';
-if (movie.trim().length === 0) {
-  res.status(400).json({
-    error: {
-      message: "Please enter a valid movie name",
-    }
-  });
-  return;
-}
