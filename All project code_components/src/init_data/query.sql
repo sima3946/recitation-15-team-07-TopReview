21247b39ae81eb7611b24e9095712e7115fdd7bf
Alexander Pham <alph4995@colorado.edu>
2023-04-26 08:41:36 -0600

    Adding Query to sort reviews by score

---
+++ All project code_components/src/init_data/query.sql
@@ -0,0 +1,15 @@
+/* QUERY TO SORT REVIEWS ACCORDING TO SENTIMENT SCORE (BAD, MEDIUM, GOOD) */
+/* BAD */
+SELECT review_id, review
+    FROM MovieReviews
+    WHERE sentimentScore 
+
+/* MEDIUM */
+SELECT review_id, review
+    FROM MovieReviews
+
+
+/* GOOD */
+SELECT review_id, review
+    FROM MovieReviews
+
