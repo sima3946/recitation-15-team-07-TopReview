a9de31be1bda93bf75e020da6234d35e88cf7d28
kive7791 <kvel1097@gmail.edu>
2023-04-27 11:13:59 -0600

    Error for name

--- All project code_components/src/index.js
+++ All project code_components/src/index.js
@@ -1,3 +1,6 @@
+// /**** CHAT TESING ****/
+// import { render } from "ejs";
+// import { useState } from "react";
 // *****************************************************
 // <!-- Section 1 : Import Dependencies -->
 // *****************************************************
@@ -10,7 +13,8 @@ const session = require('express-session'); // To set the session object. To sto
 const bcrypt = require('bcrypt'); //  To hash passwords
 // const mysql = require('mysql');
 const fetch = require('node-fetch');
-const axios = require('axios'); // To make HTTP requests from our server. We'll learn more about it in Part B.
+const axios = require('axios'); // To make HTTP requests from our server.
+// const openai = require('openai'); //Make http requests from our server to openai.
 
 // *****************************************************
 // <!-- Section 2 : Connect to DB -->
@@ -40,7 +44,7 @@ db.connect()
 // *****************************************************
 // <!-- Section 3 : App Settings -->
 // *****************************************************
-
+app.use( express.static( "resources" ) );
 app.set('view engine', 'ejs'); // set the view engine to EJS
 app.use(bodyParser.json()); // specify the usage of JSON for parsing request body.
 
@@ -66,7 +70,7 @@ app.use(
 // All API routes go here
 
 app.get('/welcome', (req, res) => {
-  res.json({ status: 'success', message: 'Welcome!' });
+  res.json({status: 'success', message: 'Welcome!'});
 });
 
 app.get('/', (req, res) => {
@@ -78,21 +82,56 @@ app.get('/login', (req, res) => {
 });
 
 app.get('/home', (req, res) => {
-  const query = `SELECT * FROM Movies LIMIT 5`
+  const query = `SELECT * FROM Movies LIMIT 6`
   db.any(query)
   .then(async movies => {
-    let titles = ['', '', '', '', '']
-    let image_urls = ['', '', '', '', '']
-    for (let i = 0; i < 5; i++) {
+    let titles = ['', '', '', '', '', '']
+    let image_urls = ['', '', '', '', '', '']
+    let movie_ids = ['', '', '', '', '', '']
+    for (let i = 0; i < 6; i++) {
       titles[i] = movies[i].name
       image_urls[i] = movies[i].image_url
+      movie_ids[i] = movies[i].movie_id
     }
-    res.render("pages/home", {names: titles, urls: image_urls});
+    res.render("pages/home", {names: titles, urls: image_urls, ids: movie_ids, status: 200, message: 'Success'});
   })
   .catch(err => {
     console.log(err);
   })
 });
+
+app.get('/review', (req, res) => {
+  movieID = '';
+  found = false;
+  for (let i = 0; i < req.url.length; i++) {
+    if (found == false) {
+      if (req.url[i] == 'i' && req.url[i + 1] == 'd') {
+        found = true;
+        i++; i++;
+      }
+    }
+    else {
+      movieID += req.url[i]
+    }
+  }
+
+  const queryMovies = `SELECT * FROM movies WHERE movie_id = '${movieID}';`
+  const queryReviews = `SELECT * FROM MovieReviews WHERE movie_id = '${movieID}';`
+
+  db.any(queryMovies)
+  .then(dataMovies => {
+    db.any(queryReviews)
+    .then(dataReviews => {
+      res.render("pages/review", {movie: dataMovies, reviews: dataReviews})
+    })
+    .catch(err2 => {
+      console.log(err2)
+    })
+  })
+  .catch(err1 => {
+    console.log(err1)
+  })
+})
 
 app.get('/profile', (req, res) => {
   res.render("pages/profile", {
@@ -104,26 +143,26 @@ app.get('/profile', (req, res) => {
 app.post('/login', async (req, res) => {
   const query = `SELECT * FROM users WHERE username = '${req.body.username}';`;
   db.one(query)
-    .then(async user => {
-      const match = await bcrypt.compare(req.body.password, user.password)
-      if (req.body.password == '' || req.body.password == 'undefined') {
-        console.log("no password");
-      }
-      else if (match) {
-        req.session.user = user;
-        req.session.save();
-        res.render('pages/home', { status: 200, message: 'Success' });
-      }
-      else {
-        res.status(201).json({ message: 'Invalid input' });
-      }
-    })
-    .catch(err => {
-      if (err.code == 0) {
-        res.render("pages/login");
-      }
-      return console.log(err);
-    });
+  .then(async user => {
+    const match = await bcrypt.compare(req.body.password, user.password)
+    if (req.body.password == '' || req.body.password == 'undefined') {
+      console.log("no password");
+    }
+    else if (match) {
+      req.session.user = user;
+      req.session.save();
+      res.redirect('/home');
+    }
+    else {
+      res.status(201).json({message: 'Invalid input'});
+    }
+  })
+  .catch(err => {
+    if (err.code == 0) {
+      res.render("pages/login");
+    }
+    return console.log(err);
+  });
 });
 
 app.get('/register', (req, res) => {
@@ -138,204 +177,272 @@ app.post('/register', async (req, res) => {
   const username = await req.body.username;
   const query = "INSERT INTO users (username, password) values ($1, $2);"
   if (req.body.password == '') {
-    res.status(201).json({ message: 'No input' })
+    res.status(201).json({message: 'No input'})
     return
   }
   else {
     db.any(query, [username, hash])
-      .then(async data => {
-        res.render('pages/login', { status: 200, message: 'Success' });
-        return
-      })
-      .catch(err => {
-        res.render('pages/register', { status: 201, message: 'Username taken' });
-        return
-      });
+    .then(async data => {
+      res.render('pages/login', {status: 200, message: 'Success'});
+      return
+    })
+    .catch(err => {
+      res.render('pages/register', {status: 201, message: 'Username taken'});
+      return
+    });
   }
-
 })
 
 app.post('/userID', async (req, res) => {
   const query = `SELECT userID FROM users WHERE username = '${req.body.username}';`
   db.one(query)
-    .then(async user => {
-      const userID = await user.userid;
-      res.status(200);
-      res.json({ userID: userID, message: 'Success' })
-    })
-    .catch(err => {
-      res.status(201);
-      res.json({ message: 'Invalid username' });
-    })
+  .then(async user => {
+    const userID = await user.userid;
+    res.status(200);
+    res.json({userID: userID, message: 'Success'})
+  })
+  .catch(err => {
+    res.status(201);
+    res.json({message: 'Invalid username'});
+  })
 });
 
-//home page + movie cards GET route
-app.get('/', (req, res) => {
-  const query = 'SELECT * FROM Movies ORDER BY RAND() LIMIT 5';
-  connection.query(query, (err, results) => {
-    if (err) {
-      throw err;
-    }
-    const movies = results;
-    res.render('home', { movies: movies });
-  });
-});
 
-// GET Endpoint for retrieving movies from the TMDb API
+
+
+const tmdb_apiKey = '32e03fbc1ac17bae20d12c4548e26ce8'; // Gunhi's TMDb API key
+
+// Endpoint to retrieve movies from TMDB and store them in the Movies table
 app.get('/movies', async (req, res) => {
   try {
-    const api_key = '32e03fbc1ac17bae20d12c4548e26ce8'; // Gunhi's TMDb API key
-    let page = 1;
-    let total_pages = 1;
-    let count = 0;
+    // Make initial API call to get total number of pages
+    const initialResponse = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${tmdb_apiKey}&language=en-US&page=1`);
+    const totalPages = initialResponse.data.total_pages;
+    const moviesPerPage = initialResponse.data.results.length;
 
-    while (page <= total_pages && (count < 1)) {
-      const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${api_key}&page=${page}`);
-      const data = await response.json();
-      const movies = data.results.map(movie => {
-        return {
-          id: movie.id,
-          title: movie.title,
-          description: movie.overview,
-          image_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`
-        };
-      });
+    // Loop through all pages of results and store movies in database
+    for (let page = 1; page <= totalPages; page++) {
+      const response = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${tmdb_apiKey}&language=en-US&page=${page}`);
+      const movies = response.data.results;
 
-      const sql = 'INSERT INTO Movies (movie_id, name, description, image_url) VALUES ($1, $2, $3, $4)';
+      // Insert each movie into the Movies table
       for (let i = 0; i < movies.length; i++) {
-        db.any(sql, [movies[i].id, movies[i].title, movies[i].description, movies[i].image_url])
-          .then(async data => {
-            res.status(200);
-          })
-          .catch(err => {
-            console.log(err);
-          })
-      }
+        const movie = movies[i];
+        const movieId = movie.id;
+        const name = movie.title;
+        const description = movie.overview;
+        const imageUrl = movie.poster_path ? `https://image.tmdb.org/t/p/w500${movie.poster_path}` : null;
+        const year = parseInt(movie.release_date.substring(0, 4));
 
-      page++;
-      total_pages = data.total_pages;
-      count++;
-    }
-
-    res.send('Movies inserted into database successfully!');
-  }
-  catch (error) {
-    console.error(error);
-    res.status(500).send('Error inserting movies into database');
-  }
-});
-
-app.get('/movieInfo', async (req, res) => {
-  const query = `SELECT * FROM Movies`;
-  db.any(query)
-    .then(async movies => {
-      console.log(movies);
-    })
-    .catch(err => {
-      console.log(err);
-    })
-});
-
-
-/* GET Endpoint for retrieving REVIEWS for movies from the TMDb API
-          and inserting them into the MovieReviews table */
-app.get('/reviews', async (req, res) => {
-  try {
-    const api_key = '32e03fbc1ac17bae20d12c4548e26ce8';
-    const movies = await db.query('SELECT movie_id FROM Movies');
-
-    for (const movie of movies) {
-      const movie_id = movie.movie_id;
-      const response = await fetch(`https://api.themoviedb.org/3/movie/${movie_id}/reviews?api_key=${api_key}`);
-      const data = await response.json();
-      const reviews = data.results.map(review => {
-        return {
-          movie_id: movie_id,
-          review: review.content,
-          sentimentScore: -2
-        };
-      });
-
-      const sql = 'INSERT INTO MovieReviews (movie_id, review, sentimentScore) VALUES ($1, $2, $3)';
-      for (let i = 0; i < reviews.length; i++) {
-        db.any(sql, [reviews[i].movie_id, reviews[i].review, reviews[i].sentimentScore])
-          .then(data => {
-            res.status(200);
-          })
-          .catch(err => {
-            console.log(err);
-          })
+        // Insert movie into database
+        await pool.query('INSERT INTO Movies (movie_id, name, description, image_url, year) VALUES ($1, $2, $3, $4, $5)', [movieId, name, description, imageUrl, year]);
       }
     }
 
-    res.send('Movie reviews inserted into database successfully!');
+    res.status(200).send('Movies successfully stored in database');
   } catch (error) {
-    console.error(error);
-    res.status(500).send('Error inserting movie reviews into database');
+    console.log(error)
   }
-});
+})
 
-//supposed to have the search bar render the page of the movie that is being searched
-app.get('/search', async (req, res) => {
+/*
+// API Key for Google Cloud Sentiment Analysis (gunhi)
+const sentiment_api_key = 'AIzaSyBFJjk7mor-E9HL4hMyaFcRI0mdhLCZaTg';
+
+// helper function to get sentiment score from Google Cloud's sentiment analysis API
+async function getSentimentScore(review) {
   try {
-    //const movie = req.body.search || '';
-    const query = "SELECT (Movies.movie_id) FROM Movies WHERE Movies.name = $1";
-    let movieLook = await db.any(query, [req.body.search]); //should be able to find the movie that is being searched for
-    res.render('pages/review', movieLook);
-    // if (movie.trim().length > 0) {
-    //   res.send('Movie successfully found!');
-    // }
-  } catch (error) {
-    res.status(400).json({
-      error: {
-        message: "Please enter a valid movie name",
-      }
+    const response = await fetch(`https://language.googleapis.com/v1/documents:analyzeSentiment?key=${sentiment_api_key}`, {
+      method: 'POST',
+      body: JSON.stringify({
+        document: {
+          type: 'PLAIN_TEXT',
+          content: review,
+        },
+        encodingType: 'UTF8',
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
     });
-    return;
+
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status} ${response.statusText}`);
+    }
+
+    const result = await response.json();
+    return result.documentSentiment.score;
+  } catch (error) {
+    console.error(error);
+    throw new Error('Error retrieving sentiment score');
+  }
+}
+
+*/
+//gets all tmdb reviews
+app.get('/tmdb-reviews', async (req, res) => {
+  try {
+    // Get all movies from database
+    const movies = await pool.query('SELECT * FROM Movies');
+
+    // Loop through each movie and get reviews from TMDB API
+    for (const movie of movies.rows) {
+      const response = await axios.get(`https://api.themoviedb.org/3/movie/${movie.tmdb_id}/reviews?api_key=${tmdb_apiKey}`);
+
+      // Loop through each review and store in database
+      for (const review of response.data.results) {
+        // Perform sentiment analysis on review
+        const sentimentResponse = await axios.post(`https://language.googleapis.com/v1/documents:analyzeSentiment?key=${sentiment_api_key}`, {
+          document: {
+            type: 'PLAIN_TEXT',
+            content: review.content
+          }
+        });
+
+        // Insert review and sentiment score into database
+        await pool.query('INSERT INTO TMDB_Reviews (movie_id, review, sentiment_score) VALUES ($1, $2, $3)', [movie.movie_id, review.content, sentimentResponse.data.documentSentiment.score]);
+
+      }
+    }
+
+    res.send('Successfully retrieved and stored TMDB reviews');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Server error');
   }
 });
+
+// Endpoint to get Letterboxd reviews for movies already in database
+app.get('/letterboxd/reviews', async (req, res) => {
+  try {
+    // Get movies from database
+    const result = await pool.query('SELECT * FROM Movies');
+    const movies = result.rows;
+    
+    // Loop through movies and get reviews
+    for (const movie of movies) {
+      // Make request to Letterboxd API to get reviews for movie
+      const response = await axios.get(`https://api.letterboxd.com/api/v0.1/films/${movie.movie_id}/reviews?perpage=100`);
+      const reviews = response.data.items;
+      // Loop through reviews and insert into database with sentiment score
+      for (const review of reviews) {
+        const sentimentScore = await getSentimentScore(review.body);
+        const values = [movie.movie_id, review.body, sentimentScore];
+        await pool.query('INSERT INTO Letterboxd_Reviews (movie_id, review, sentiment_score) VALUES ($1, $2, $3)', values);
+      }
+    }
+    res.send('Letterboxd reviews successfully retrieved and stored.');
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error retrieving and storing Letterboxd reviews.');
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+          
 app.get('/reviewInfo', async (req, res) => {
   const query1 = `SELECT * FROM MovieReviews`;
   const query2 = `SELECT * FROM MovieReviews ORDER BY movie_id ;`
   db.any(query1)
-    .then(async movies => {
-      console.log(movies);
-    })
-    .catch(err => {
-      console.log(err);
-    })
+  .then(async movies => {
+    console.log(movies);
+  })
+  .catch(err => {
+    console.log(err);
+  })
 })
 
-app.get('/logout', (req, res) => {
-  req.session.destroy();
-  res.render('pages/login', { message: 'Logged out Successfully' });
-});
-
-//supposed to have the search bar render the page of the movie that is being searched
-app.get('/search', async (req, res) => {
-  try {
-    //const movie = req.body.search || '';
-    const query = "SELECT (Movies.movie_id) FROM Movies WHERE Movies.name = $1";
-    let movieLook = await db.any(query, [req.body.search]); //should be able to find the movie that is being searched for
-    res.render('pages/review', movieLook);
-    // if (movie.trim().length > 0) {
-    //   res.send('Movie successfully found!');
-    // }
-  } catch (error) {
-    res.status(400).json({
-      error: {
-        message: "Please enter a valid movie name",
+/*
+app.get('/scores', async(req, res) => {
+  try{
+    const connection = await mysql.createConnection(dbConfig);
+    const [rows] = await connection.execute('SELECT id, reviews FROM movies');
+  
+    // Loop through the movies and their reviews, and make an API call for each review
+    for (const row of rows) {
+      const movieId = row.id;
+      const reviews = JSON.parse(row.reviews);
+  
+      for (const review of reviews) {
+        const reviewText = review.text;
+        const apiUrl = `${sentimentApiUrl}?text=${reviewText}`;
+  
+        try {
+          const response = await axios.get(apiUrl);
+          const sentiment = response.data.sentiment;
+          console.log(`Sentiment analysis result for review '${reviewText}' in movie ${movieId}: ${sentiment}`);
+          // Update the review in the database with the sentiment analysis result
+          await connection.execute('UPDATE movies SET reviews = JSON_SET(reviews, CONCAT("$[", JSON_SEARCH(reviews, "one", ?), "].sentiment"), ?) WHERE id = ?', [reviewText, sentiment, movieId]);
+        } catch (error) {
+          console.error(`Error analyzing sentiment for review '${reviewText}' in movie ${movieId}: ${error}`);
+        }
       }
-    });
-    return;
+    }
+  } catch(error) {
+    console.error(error);
+    res.status(500).send('Error giving reviews sentiment score');
   }
 });
 
-// import { render } from "ejs";
-// /**** CHAT TESING ****/
-// import { useState } from "react";
+async function sortReviewsBySentiment() {
+  // Connect to the database and retrieve the reviews
+  const connection = await mysql.createConnection(dbConfig);
+  const [rows] = await connection.execute('SELECT sentiment_score FROM reviews');
 
-// export default function Home() {
+  // Loop through the reviews and group them into bad, medium, and good categories based on their sentiment score
+  const badReviews = [];
+  const mediumReviews = [];
+  const goodReviews = [];
+
+  for (const row of rows) {
+    const sentimentScore = row.sentiment_score;
+
+    if (sentimentScore === null || sentimentScore === undefined) {
+      continue;
+    }
+
+    if (sentimentScore < -0.33) {
+      badReviews.push(row);
+    } else if (sentimentScore >= -0.33 && sentimentScore <= 0.33) {
+      mediumReviews.push(row);
+    } else {
+      goodReviews.push(row);
+    }
+  }
+
+  // Print the results
+  console.log(`Bad reviews (${badReviews.length}):`);
+  console.log(badReviews.map(review => review.id));
+
+  console.log(`Medium reviews (${mediumReviews.length}):`);
+  console.log(mediumReviews.map(review => review.id));
+
+  console.log(`Good reviews (${goodReviews.length}):`);
+  console.log(goodReviews.map(review => review.id));
+
+  // Close the database connection
+  await connection.end();
+}
+
+sortReviewsBySentiment();
+*/
+
+
+/**** CHAT TESING ****/
+
+// function Home ()
+// {
 //   const [movieInput, setMovieInput] = useState("");
 //   const [result, setResult] = useState();
 //   //This submit event will be for when the user selects the movie card on the homepage
@@ -364,7 +471,7 @@ app.get('/search', async (req, res) => {
 //     }
 //   }
 //   res.render('pages/reviews')
-// }
+// };
 
 // *****************************************************
 // <!-- Section 5 : Start Server-->
