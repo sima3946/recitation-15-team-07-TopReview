f15e2e32d173c011b3c4c8da1d9322329716a570
sima3946 <sima3946@colorado.edu>
2023-05-03 16:15:20 -0600

    checking something to see if it will work on azure

--- All project code_components/src/resources/js/script.js
+++ All project code_components/src/resources/js/script.js
@@ -1,5 +1,5 @@
 function initializeContent(){
-    let response = fetch('http://localhost:3000/movies');
+    let response = fetch('http://recitation-015-team-07.eastus.cloudapp.azure.com:3000/movies');
     console.log(response.status)
     console.log('in initializeContent function');
 }
